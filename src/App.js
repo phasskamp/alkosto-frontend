b8630b4c81ef6b2ddc71534a542be3f5ebe4ca6e
Philipp Hasskamp <philipphasskamp@Mac-mini-von-Philipp.local>
2025-06-10 18:19:16 -0500

    Fix App.js to show Alkosto component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import AlkostoProductAdvisor from './AlkostoProductAdvisor';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AlkostoProductAdvisor />
+    </div>
+  );
+}
+
+export default App;import React from 'react';
 import './App.css';
 import AlkostoProductAdvisor from './AlkostoProductAdvisor';
 
