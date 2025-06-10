a83a4ff5afb5f291ee7106f6695ac7123c2d7a05
Philipp Hasskamp <philipphasskamp@Mac-mini-von-Philipp.local>
2025-06-10 18:26:18 -0500

    Fix App.js - remove duplicate code

--- src/App.js
+++ src/App.js
@@ -10,16 +10,4 @@ function App() {
   );
 }
 
-export default App;import React from 'react';
-import './App.css';
-import AlkostoProductAdvisor from './AlkostoProductAdvisor';
-
-function App() {
-  return (
-    <div className="App">
-      <AlkostoProductAdvisor />
-    </div>
-  );
-}
-
 export default App;
